310486308b9dcc80bc06f0dc154c995de1a34d94
dduartess <dandandsoares@gmail.com>
2026-05-07 23:17:29 -0300

    feat: add in-memory item repository

--- InventoryControl.Infrastructure/Repositories/InMemoryItemRepository.cs
+++ InventoryControl.Infrastructure/Repositories/InMemoryItemRepository.cs
@@ -1,12 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using InventoryControl.Application.Interfaces;
+using InventoryControl.Domain.Entities;
 
-namespace InventoryControl.Infrastructure.Repositories
+namespace InventoryControl.Infrastructure.Repositories;
+
+public class InMemoryItemRepository : IItemRepository
 {
-    public class InMemoryItemRepository
+    private readonly List<Item> _items = new();
+
+    public List<Item> GetAllItems()
     {
-        
+        return _items;
+    }
+
+    public Item? GetItemById(int id)
+    {
+        return _items.FirstOrDefault(item => item.Id == id);
+    }
+
+    public Item CreateItem(Item item)
+    {
+        var nextId = _items.Count + 1;
+
+        item.SetId(nextId);
+
+        _items.Add(item);
+
+        return item;
+    }
+
+    public bool UpdateItem(Item item)
+    {
+        var existingItem = GetItemById(item.Id);
+
+        if (existingItem is null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool DeleteItem(int id)
+    {
+        var item = GetItemById(id);
+
+        if (item is null)
+        {
+            return false;
+        }
+
+        _items.Remove(item);
+
+        return true;
     }
 }
